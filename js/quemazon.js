0d9cd123f97f9aa00d2f20527998323500c9bfa8
geohouse <geo.house7@gmail.com>
2022-04-17 12:10:59 -0600

    trail mapping added with scene selection

--- js/quemazon.js
+++ js/quemazon.js
@@ -1,5 +1,5 @@
-// Left Hand Creek area mapping
-var map = L.map('map').setView([40.149408,-105.094659],19);
+// Quemazon area mapping
+var map = L.map('map').setView([35.884460,-106.333577],18);
 
 // Need to set the max zoom to 21 here because the tiles go that fine but Leaflet stops at 18 by default.
 
@@ -27,9 +27,7 @@ customBackgroundSelection.onAdd = function(map){
 
 customBackgroundSelection.addTo(map);
 
-
 let backgroundSelector = document.getElementById("background-select");
-let sliderElement = document.getElementById("slider");
 
 function getSelectedBackgroundName(){
     let data = new FormData(backgroundSelector);
@@ -44,30 +42,109 @@ function getSelectedBackgroundName(){
 }
 
 
-function createTileLayerURL(tileTypeSelection, monthSelection){
 
-    let tileLayerURL = "https://geohouse.github.io/droneMapping/mapTiles/" + monthSelection + "/" + tileTypeSelection + "/{z}/{x}/{-y}.png";
+var customSceneSelection = L.control();
+
+customSceneSelection.update = function(properties){
+    this._div.innerHTML = '<label id="scene-label for=scene-select">Choose an overlapping scene to view</label>' + 
+    '<form id = "scene-select">' + 
+    '    <input type="checkbox" id="canyons" name="scene-type" value="CanyonsOnly" checked>' + 
+    '    <label for="canyons">Show canyons scene</label>' + 
+    '    <input type="checkbox" id="trails" name="scene-type" value="TrailsOnly" checked>' + 
+    '    <label for="trails">Show trails scene</label>' + 
+    '</form>' + 
+    '</div>';
+};
+    
+customSceneSelection.onAdd = function(map){
+    this._div = L.DomUtil.create('div', 'scene-div');
+    this.update();
+
+    // Normally a double click causes Leaflet to zoom to where is double clicked. This removes that
+    // functionality from the buttons, because a double click can trigger (and therefore move/zoom the map)
+    // when just trying to compare the float types and clicking fairly quickly. This doesn't affect the 
+    // ability to double click to zoom anywhere else on the map. 
+    // From: https://gist.github.com/rdaly1490/eb98fc5ff5be253c5610
+    this._div.ondblclick = (e) => {
+        e.stopPropagation();
+        console.log("double clicked");
+    };
+        return this._div;
+    };
+    
+
+customSceneSelection.addTo(map);
+
+function generateTileURL(selectedBackground, mapLocation){
+    // For the canyons flight area
+    let tileLayerURL = "https://geohouse.github.io/droneMapping/mapTiles/" + mapLocation + "/" + selectedBackground + "/{z}/{x}/{-y}.png";
+    console.log(tileLayerURL);
     return tileLayerURL;
 }
 
-
-function createMapBackground(){
+// These will be used in createMapBackgrounds to get the selected scenes
+let canyonSceneCheck = document.getElementById("canyons");
+let trailSceneCheck = document.getElementById("trails");
+let trailLayer;
+let canyonLayer;
+function createMapBackgrounds(){
 
     // Remove any current background layer if one exists.
-    if(currentBackgroundLayer != undefined){
-        map.removeLayer(currentBackgroundLayer);
+    if(canyonLayer != undefined){
+        map.removeLayer(canyonLayer);
     }
 
+    if(trailLayer != undefined){
+        map.removeLayer(trailLayer);
+    }
+    let selectedBackgroundType = getSelectedBackgroundName();
+
     // Cap the zoom range at what the tiles support
+    // Plot the trails layer first (under the canyons tile layer) for the DSM, 
+    // And the canyons layer first (under the trails tile layer) for the ortho photo.
     
-    currentBackgroundLayer = L.tileLayer(tileLayerURL, {
-                maxZoom:21,
-                attribution: 'Map tiles made with <a href="https://www.opendronemap.org">OpenDroneMap</a>, using drone images collected by <a href="https://github.com/geohouse">geohouse</a>'
-    });
-    currentBackgroundLayer.addTo(map);
+    if(canyonSceneCheck.checked){
+        let tileURL_canyons = generateTileURL(selectedBackgroundType, "Quemazon_canyons_032822");
+        canyonLayer = L.tileLayer(tileURL_canyons, {
+                    maxZoom:21,
+                    attribution: 'Map tiles made with <a href="https://www.opendronemap.org">OpenDroneMap</a>, using drone images collected by <a href="https://github.com/geohouse">geohouse</a>'
+        });
+ 
+    } 
+
+    if(trailSceneCheck.checked){
+        let tileURL_trail = generateTileURL(selectedBackgroundType, "Quemazon_trail_032822");
+        trailLayer = L.tileLayer(tileURL_trail, {
+            maxZoom:21,
+            attribution: 'Map tiles made with <a href="https://www.opendronemap.org">OpenDroneMap</a>, using drone images collected by <a href="https://github.com/geohouse">geohouse</a>'
+        });
+    }
+
+    if(trailSceneCheck.checked && canyonSceneCheck.checked && selectedBackgroundType == "orthomosaic"){
+        canyonLayer.addTo(map);
+        trailLayer.addTo(map);
+    }
+
+    if(trailSceneCheck.checked && canyonSceneCheck.checked && selectedBackgroundType == "surfaceModel"){
+        trailLayer.addTo(map);
+        canyonLayer.addTo(map);
+    }
+
+    if(trailSceneCheck.checked){
+        trailLayer.addTo(map);
+    }
+
+    if(canyonSceneCheck.checked){
+        canyonLayer.addTo(map);
+    }
+
+
 }
 
-backgroundSelector.addEventListener("change", createMapBackground);
-sliderElement.addEventListener("change", createMapBackground);
+backgroundSelector.addEventListener("change", createMapBackgrounds);
 
-createMapBackground();
+canyonSceneCheck.addEventListener("change", createMapBackgrounds);
+trailSceneCheck.addEventListener("change", createMapBackgrounds);
+
+
+createMapBackgrounds();
