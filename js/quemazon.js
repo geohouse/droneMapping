f9c0a644ef6291c0f7ddd361caf23c5e1aab3346
geohouse <geo.house7@gmail.com>
2022-04-17 11:08:04 -0600

    Starting to add additional maps

---
+++ js/quemazon.js
@@ -0,0 +1,73 @@
+// Left Hand Creek area mapping
+var map = L.map('map').setView([40.149408,-105.094659],19);
+
+// Need to set the max zoom to 21 here because the tiles go that fine but Leaflet stops at 18 by default.
+
+var customBackgroundSelection = L.control();
+var currentBackgroundLayer;
+
+customBackgroundSelection.update = function(properties){
+    this._div.innerHTML = '<label id="background-label for=background-select">Choose a map to view</label>' + 
+    '<form id = "background-select">' + 
+    '    <input type="radio" id="orthomosaic" name="map-type" value="orthomosaic" checked>' + 
+    '    <label for="orthomosaic">Orthomosaic</label>' + 
+    '    <input type="radio" id="surfaceModel" name="map-type" value="surfaceModel">' + 
+    '    <label for="surfaceModel">Digital Surface Model</label>' + 
+    '</form>' + 
+    '</div>';
+};
+    
+customBackgroundSelection.onAdd = function(map){
+    this._div = L.DomUtil.create('div', 'background-div');
+    this.update();
+    
+        return this._div;
+    };
+    
+
+customBackgroundSelection.addTo(map);
+
+
+let backgroundSelector = document.getElementById("background-select");
+let sliderElement = document.getElementById("slider");
+
+function getSelectedBackgroundName(){
+    let data = new FormData(backgroundSelector);
+    var selectedBackground = "";
+    for (const entry of data){
+            selectedBackground = entry[1];
+    }  
+    console.log("The selected background is:");
+    // Should be either 'orthomosaic' or 'surfaceModel'
+    console.log(selectedBackground);
+    return selectedBackground;
+}
+
+
+function createTileLayerURL(tileTypeSelection, monthSelection){
+
+    let tileLayerURL = "https://geohouse.github.io/droneMapping/mapTiles/" + monthSelection + "/" + tileTypeSelection + "/{z}/{x}/{-y}.png";
+    return tileLayerURL;
+}
+
+
+function createMapBackground(){
+
+    // Remove any current background layer if one exists.
+    if(currentBackgroundLayer != undefined){
+        map.removeLayer(currentBackgroundLayer);
+    }
+
+    // Cap the zoom range at what the tiles support
+    
+    currentBackgroundLayer = L.tileLayer(tileLayerURL, {
+                maxZoom:21,
+                attribution: 'Map tiles made with <a href="https://www.opendronemap.org">OpenDroneMap</a>, using drone images collected by <a href="https://github.com/geohouse">geohouse</a>'
+    });
+    currentBackgroundLayer.addTo(map);
+}
+
+backgroundSelector.addEventListener("change", createMapBackground);
+sliderElement.addEventListener("change", createMapBackground);
+
+createMapBackground();
