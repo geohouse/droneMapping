ad1e6317c4ce33638670b7785c4414617f187097
Geoffrey House <geo.house7@gmail.com>
2022-07-26 10:10:44 -0600

    Adding Homestead Potree 3-D data

---
+++ potreeData/Irvin-Homestead-PotreeConvert1-7-0725222/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 7115981,
+    "boundingBox": {
+        "lx": 462608.163,
+        "ly": 4462844.827,
+        "lz": 2614.741,
+        "ux": 462736.7380000002,
+        "uy": 4462973.402,
+        "uz": 2743.3160000001862
+    },
+    "tightBoundingBox": {
+        "lx": 462608.16273880007,
+        "ly": 4462844.82651138,
+        "lz": 2614.74145507813,
+        "ux": 462734.9507388,
+        "uy": 4462973.4015113799,
+        "uz": 2645.36845507813
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.1134921312332154,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
---
+++ potreeData/Laycook-House-Homestead-PotreeConvert1-7-0725222/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 10540508,
+    "boundingBox": {
+        "lx": 461351.048,
+        "ly": 4460394.558,
+        "lz": 2520.339,
+        "ux": 461518.29,
+        "uy": 4460561.8,
+        "uz": 2687.580999999969
+    },
+    "tightBoundingBox": {
+        "lx": 461351.04842376706,
+        "ly": 4460394.55758667,
+        "lz": 2520.33911132812,
+        "ux": 461518.290423767,
+        "uy": 4460550.868586671,
+        "uz": 2550.42011132812
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 1.448358178138733,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
