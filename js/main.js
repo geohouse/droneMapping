a33e23dc749c6759a1e63c30b4c0c4446f68deb1
Geoffrey House <geo.house7@gmail.com>
2022-07-26 21:52:39 -0600

    Adding homestead 3-D models

--- js/main.js
+++ js/main.js
@@ -1,42 +1,45 @@
 // From https://www.w3schools.com/howto/howto_js_portfolio_filter.asp
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+filterSelection("all");
+function filterSelection(labelSelection) {
+  const columnElements = document.getElementsByClassName("column");
+  if (labelSelection === "all") {
+    labelSelection = "";
+  }
+  console.log(columnElements);
+  for (let index = 0; index < columnElements.length; index++) {
+    w3RemoveClass(columnElements[index], "show");
+    if (columnElements[index].className.indexOf(labelSelection) > -1) {
+      w3AddClass(columnElements[index], "show");
+    }
   }
 }
 
 function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  const arr1 = element.className.split(" ");
+  const arr2 = name.split(" ");
+  for (let index = 0; index < arr2.length; index++) {
+    if (arr1.indexOf(arr2[index]) === -1) {
+      element.className += " " + arr2[index];
+    }
   }
 }
 
 function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+  const arr1 = element.className.split(" ");
+  const arr2 = name.split(" ");
+  for (let index = 0; index < arr2.length; index++) {
+    while (arr1.indexOf(arr2[index]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[index]), 1);
     }
   }
   element.className = arr1.join(" ");
 }
 
-
 // Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
+const btnContainer = document.getElementById("myBtnContainer");
+const btns = btnContainer.getElementsByClassName("btn");
+for (let index = 0; index < btns.length; index++) {
+  btns[index].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
