27fed5db1ef1d47274c4c4a78dee41bae5433640
Geoffrey House <geo.house7@gmail.com>
2022-11-06 17:02:20 -0700

    Fully re-worked date selectors

--- js/leftHand.js
+++ js/leftHand.js
@@ -1,5 +1,5 @@
 // Left Hand Creek area mapping
-var map = L.map("map").setView([40.149408, -105.094659], 19);
+var map = L.map("map").setView([40.149408, -105.0942], 19);
 
 // Need to set the max zoom to 21 here because the tiles go that fine but Leaflet stops at 18 by default.
 
@@ -8,7 +8,7 @@ var currentBackgroundLayer;
 
 customBackgroundSelection.update = function (properties) {
   this._div.innerHTML =
-    '<label id="background-label for=background-select">Choose a map to view</label>' +
+    '<label id="background-label" for="background-select">Choose a map to view</label>' +
     '<form id = "background-select">' +
     '    <input type="radio" id="orthomosaic" name="map-type" value="orthomosaic" checked>' +
     '    <label for="orthomosaic">Orthomosaic</label>' +
@@ -27,86 +27,84 @@ customBackgroundSelection.onAdd = function (map) {
 
 customBackgroundSelection.addTo(map);
 
-var customControlYearSlider = L.control();
+// var customControlYearSlider = L.control();
 
-customControlYearSlider.update = function (properties) {
-  this._div.innerHTML =
-    '<label id="slide-label" for="slider">Month to map (2022)</label>' +
-    '<input type = "range" id = "slider" name = "slider" min="1" max="11" step="1" value="1">' +
-    '<div class="sliderTicks">' +
-    '<p class="sliderTick">Mar.</p>' +
-    '<p class="sliderTick">Apr.</p>' +
-    '<p class="sliderTick">May</p>' +
-    '<p class="sliderTick">May</p>' +
-    '<p class="sliderTick">May</p>' +
-    '<p class="sliderTick">May</p>' +
-    '<p class="sliderTick">Jun.</p>' +
-    '<p class="sliderTick">Jul.</p>' +
-    '<p class="sliderTick">Aug.</p>' +
-    '<p class="sliderTick">Sep.</p>' +
-    '<p class="sliderTick">Oct.</p>' +
-    "</div>" +
-    '<div class="sliderTicks">' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">-----</p>' +
-    '<p class="sliderTick">-----</p>' +
-    '<p class="sliderTick">-----</p>' +
-    '<p class="sliderTick">-----</p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    "</div>" +
-    '<div class="sliderTicks">' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">1st wk.</p>' +
-    '<p class="sliderTick">2nd wk.</p>' +
-    '<p class="sliderTick">3rd wk.</p>' +
-    '<p class="sliderTick">4th wk.</p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    '<p class="sliderTick">     </p>' +
-    "</div>";
-};
+// customControlYearSlider.update = function (properties) {
+//   this._div.innerHTML =
+//     '<label id="slide-label" for="slider">Month to map (2022)</label>' +
+//     '<input type = "range" id = "slider" name = "slider" min="1" max="11" step="1" value="1">' +
+//     '<div class="sliderTicks">' +
+//     '<p class="sliderTick">Mar.</p>' +
+//     '<p class="sliderTick">Apr.</p>' +
+//     '<p class="sliderTick">May</p>' +
+//     '<p class="sliderTick">May</p>' +
+//     '<p class="sliderTick">May</p>' +
+//     '<p class="sliderTick">May</p>' +
+//     '<p class="sliderTick">Jun.</p>' +
+//     '<p class="sliderTick">Jul.</p>' +
+//     '<p class="sliderTick">Aug.</p>' +
+//     '<p class="sliderTick">Sep.</p>' +
+//     '<p class="sliderTick">Oct.</p>' +
+//     "</div>" +
+//     '<div class="sliderTicks">' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">-----</p>' +
+//     '<p class="sliderTick">-----</p>' +
+//     '<p class="sliderTick">-----</p>' +
+//     '<p class="sliderTick">-----</p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     "</div>" +
+//     '<div class="sliderTicks">' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">1st wk.</p>' +
+//     '<p class="sliderTick">2nd wk.</p>' +
+//     '<p class="sliderTick">3rd wk.</p>' +
+//     '<p class="sliderTick">4th wk.</p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     '<p class="sliderTick">     </p>' +
+//     "</div>";
+// };
 
-customControlYearSlider.onAdd = function (map) {
-  this._div = L.DomUtil.create("div", "slider-div");
-  this.update();
-  // Disable map dragging when clicking and dragging within the year slider box (makes it so the slider selects, but
-  // doesn't pan the map at the same time)
-  this._div.onmousedown = (e) => {
-    map.dragging.disable();
-    //console.log("selected in slider");
-  };
-  this._div.onmouseup = () => {
-    map.dragging.enable();
-    //console.log("selected in slider");
-  };
-  this._div.onmouseover = () => {
-    map.dragging.disable();
-    //console.log("selected in slider");
-  };
-  this._div.onmouseout = () => {
-    map.dragging.enable();
-    //console.log("selected in slider");
-  };
-  return this._div;
-};
+// customControlYearSlider.onAdd = function (map) {
+//   this._div = L.DomUtil.create("div", "slider-div");
+//   this.update();
+//   // Disable map dragging when clicking and dragging within the year slider box (makes it so the slider selects, but
+//   // doesn't pan the map at the same time)
+//   this._div.onmousedown = (e) => {
+//     map.dragging.disable();
+//     //console.log("selected in slider");
+//   };
+//   this._div.onmouseup = () => {
+//     map.dragging.enable();
+//     //console.log("selected in slider");
+//   };
+//   this._div.onmouseover = () => {
+//     map.dragging.disable();
+//     //console.log("selected in slider");
+//   };
+//   this._div.onmouseout = () => {
+//     map.dragging.enable();
+//     //console.log("selected in slider");
+//   };
+//   return this._div;
+// };
 
-customControlYearSlider.addTo(map);
-
-// Experimental
+// customControlYearSlider.addTo(map);
 
 const dateButtonSelection = L.control();
 
 dateButtonSelection.update = function (properties) {
   this._div.innerHTML = `
-  <p>Month to map (2022)</p>
+  <p id="month-select-label">Month to map (2022)</p>
   <div class="date-button-div">
       <button type="button" class="date-select">Mar</button>
       <button type="button" class="date-select">Apr</button>
@@ -155,55 +153,53 @@ function getSelectedMonth() {
   // has the class .active on it (only 1 date can have the
   // .active class at a time)
 
-  // Have to spread the nodeList to an array for filter to work.
-  let dateToPlot = Array.from(dateButtons).filter((dateButton) => {
+  // Return the march date by default
+  let dateToPlot = "Mar";
+
+  for (let dateButton of Array.from(dateButtons)) {
+    console.log({ dateButton });
     if (dateButton.classList.contains("active")) {
-      return dateButton.innerText;
-    } else {
-      // Return the March date by default
-      return "Mar";
+      dateToPlot = dateButton.innerText;
     }
-  });
+  }
 
   console.log(`Date to plot is: ${dateToPlot}`);
 
-  // Will be either 'Mar. 2022' or 'Apr. 2022'
   //let dateToPlot = document.getElementById("slider").value;
   // This is the name of the folder in both the ortho and the DSM
   // tile locations that contains the tiles for the selected month
-  console.log("Date to plot is: " + dateToPlot);
   let tileFolderNameForDate = "";
-  if (dateToPlot == "1") {
+  if (dateToPlot == "Mar") {
     tileFolderNameForDate = "Longmont_030422";
   }
-  if (dateToPlot == "2") {
+  if (dateToPlot == "Apr") {
     tileFolderNameForDate = "Longmont_040822";
   }
-  if (dateToPlot == "3") {
+  if (dateToPlot == "May Wk 1") {
     tileFolderNameForDate = "Longmont_050322";
   }
-  if (dateToPlot == "4") {
+  if (dateToPlot == "May Wk 2") {
     tileFolderNameForDate = "Longmont_051022";
   }
-  if (dateToPlot == "5") {
+  if (dateToPlot == "May Wk 3") {
     tileFolderNameForDate = "Longmont_051822";
   }
-  if (dateToPlot == "6") {
+  if (dateToPlot == "May Wk 4") {
     tileFolderNameForDate = "Longmont_052622";
   }
-  if (dateToPlot == "7") {
+  if (dateToPlot == "Jun") {
     tileFolderNameForDate = "Longmont_060622";
   }
-  if (dateToPlot == "8") {
+  if (dateToPlot == "Jul") {
     tileFolderNameForDate = "Longmont_072822";
   }
-  if (dateToPlot == "9") {
+  if (dateToPlot == "Aug") {
     tileFolderNameForDate = "Longmont_082622";
   }
-  if (dateToPlot == "10") {
+  if (dateToPlot == "Sep") {
     tileFolderNameForDate = "Longmont_091922";
   }
-  if (dateToPlot == "11") {
+  if (dateToPlot == "Oct") {
     tileFolderNameForDate = "Longmont_101422";
   }
   return tileFolderNameForDate;
@@ -251,7 +247,7 @@ dateButtons.forEach((dateButton) => {
 });
 
 backgroundSelector.addEventListener("change", createMapBackground);
-sliderElement.addEventListener("change", createMapBackground);
+// sliderElement.addEventListener("change", createMapBackground);
 /*
 currentBackgroundLayer = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}',{
     attribution: 'Map tiles by <a href="https://usgs.gov">Department of Interior/USGS</a>',
