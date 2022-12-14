d92163923da02060e68f85adee81c33058fd1131
Geoffrey House <geo.house7@gmail.com>
2022-12-13 21:31:30 -0700

    Refactored button highlight

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// From https://www.w3schools.com/howto/howto_js_portfolio_filter.asp
-
 function filterSelection(labelSelection) {
   const columnElements = document.querySelectorAll(".column");
 
   console.log(columnElements);
+  // Do the filtering by adding/removing the 'show' class from the different
+  // columns (safe to add even if it's already there, and safe to remove if it's already gone)
   for (let columnElement of columnElements) {
     if (labelSelection === "all") {
       columnElement.classList.add("show");
@@ -14,43 +14,13 @@ function filterSelection(labelSelection) {
     }
   }
 
-  //   for (let index = 0; index < columnElements.length; index++) {
-  //     w3RemoveClass(columnElements[index], "show");
-  //     if (columnElements[index].className.indexOf(labelSelection) > -1) {
-  //       w3AddClass(columnElements[index], "show");
-  //     }
-  //   }
-  // }
-
-  // function w3AddClass(element, name) {
-  //   const arr1 = element.className.split(" ");
-  //   const arr2 = name.split(" ");
-  //   for (let index = 0; index < arr2.length; index++) {
-  //     if (arr1.indexOf(arr2[index]) === -1) {
-  //       element.className += " " + arr2[index];
-  //     }
-  //   }
-  // }
-
-  // function w3RemoveClass(element, name) {
-  //   const arr1 = element.className.split(" ");
-  //   const arr2 = name.split(" ");
-  //   for (let index = 0; index < arr2.length; index++) {
-  //     while (arr1.indexOf(arr2[index]) > -1) {
-  //       arr1.splice(arr1.indexOf(arr2[index]), 1);
-  //     }
-  //   }
-  //   element.className = arr1.join(" ");
-  // }
-
   // Add active class to the current button (highlight it)
-  const btnContainer = document.querySelector("#myBtnContainer");
-  const btns = btnContainer.querySelectorAll(".btn");
-  for (let index = 0; index < btns.length; index++) {
-    btns[index].addEventListener("click", function () {
-      var current = document.querySelectorAll(".active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
+  const buttons = document.querySelectorAll(".btn");
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      let currentActive = document.querySelector(".active");
+      currentActive.classList.remove("active");
+      button.classList.add("active");
     });
   }
 }
