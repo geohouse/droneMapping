7f893959815faa9859eb624a3a7572a6bf6f57c5
Geoffrey House <geo.house7@gmail.com>
2022-12-13 21:26:29 -0700

    Refactored project filtering

--- js/main.js
+++ js/main.js
@@ -1,47 +1,58 @@
 // From https://www.w3schools.com/howto/howto_js_portfolio_filter.asp
-filterSelection("all");
+
 function filterSelection(labelSelection) {
-  const columnElements = document.getElementsByClassName("column");
-  if (labelSelection === "all") {
-    labelSelection = "";
-  }
+  const columnElements = document.querySelectorAll(".column");
+
   console.log(columnElements);
-  for (let index = 0; index < columnElements.length; index++) {
-    w3RemoveClass(columnElements[index], "show");
-    if (columnElements[index].className.indexOf(labelSelection) > -1) {
-      w3AddClass(columnElements[index], "show");
+  for (let columnElement of columnElements) {
+    if (labelSelection === "all") {
+      columnElement.classList.add("show");
+    } else if (columnElement.classList.contains(labelSelection)) {
+      columnElement.classList.add("show");
+    } else {
+      columnElement.classList.remove("show");
     }
+  }
+
+  //   for (let index = 0; index < columnElements.length; index++) {
+  //     w3RemoveClass(columnElements[index], "show");
+  //     if (columnElements[index].className.indexOf(labelSelection) > -1) {
+  //       w3AddClass(columnElements[index], "show");
+  //     }
+  //   }
+  // }
+
+  // function w3AddClass(element, name) {
+  //   const arr1 = element.className.split(" ");
+  //   const arr2 = name.split(" ");
+  //   for (let index = 0; index < arr2.length; index++) {
+  //     if (arr1.indexOf(arr2[index]) === -1) {
+  //       element.className += " " + arr2[index];
+  //     }
+  //   }
+  // }
+
+  // function w3RemoveClass(element, name) {
+  //   const arr1 = element.className.split(" ");
+  //   const arr2 = name.split(" ");
+  //   for (let index = 0; index < arr2.length; index++) {
+  //     while (arr1.indexOf(arr2[index]) > -1) {
+  //       arr1.splice(arr1.indexOf(arr2[index]), 1);
+  //     }
+  //   }
+  //   element.className = arr1.join(" ");
+  // }
+
+  // Add active class to the current button (highlight it)
+  const btnContainer = document.querySelector("#myBtnContainer");
+  const btns = btnContainer.querySelectorAll(".btn");
+  for (let index = 0; index < btns.length; index++) {
+    btns[index].addEventListener("click", function () {
+      var current = document.querySelectorAll(".active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
   }
 }
 
-function w3AddClass(element, name) {
-  const arr1 = element.className.split(" ");
-  const arr2 = name.split(" ");
-  for (let index = 0; index < arr2.length; index++) {
-    if (arr1.indexOf(arr2[index]) === -1) {
-      element.className += " " + arr2[index];
-    }
-  }
-}
-
-function w3RemoveClass(element, name) {
-  const arr1 = element.className.split(" ");
-  const arr2 = name.split(" ");
-  for (let index = 0; index < arr2.length; index++) {
-    while (arr1.indexOf(arr2[index]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[index]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-const btnContainer = document.getElementById("myBtnContainer");
-const btns = btnContainer.getElementsByClassName("btn");
-for (let index = 0; index < btns.length; index++) {
-  btns[index].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+filterSelection("all");
