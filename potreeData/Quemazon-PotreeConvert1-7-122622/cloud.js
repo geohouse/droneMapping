571d4f65b5af1206e314cbd4c961a2249f6611d6
Geoffrey House <geo.house7@gmail.com>
2022-12-27 11:28:35 -0700

    Adding canyon 3-d model potree

---
+++ potreeData/Quemazon-PotreeConvert1-7-122622/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 16593732,
+    "boundingBox": {
+        "lx": 377795.738,
+        "ly": 3971539.435,
+        "lz": 2284.202,
+        "ux": 378524.144,
+        "uy": 3972267.841,
+        "uz": 3012.6079999999592
+    },
+    "tightBoundingBox": {
+        "lx": 377795.738342285,
+        "ly": 3971539.4345092799,
+        "lz": 2284.2023925781295,
+        "ux": 378524.144342285,
+        "uy": 3972143.51750928,
+        "uz": 2503.99039257813
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 6.308180809020996,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
